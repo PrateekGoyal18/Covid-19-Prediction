4f1eea8d29c19cb90e0a128000a7f73d509c5dae
pkjmesra <pkjmesra@gmail.com>
2020-05-17 11:23:19 +0530

    Update app.py
    Changed the IP to default to localhost so it does not fail when someone forks and runs this without making changes.

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ def handle_exception(e):
     return render_template("error_page.html", e=e, errorCode='500'), 500
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.14', debug=True)
+    app.run(host='127.0.0.1', debug=True)
